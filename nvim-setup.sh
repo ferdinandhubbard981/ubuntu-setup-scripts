79815b8f6b8d6bd4c9c79f41c2f3cee9e9be5fe5
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2023-08-24 16:14:53 +0200

    fixed nvim setup and minor fix in ubuntu script

---
+++ nvim-setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+
+#### building and installing neovim
+echo "SETTING UP neovim"
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+rm -rf neovim
+git clone https://github.com/neovim/neovim
+cd neovim && git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+
+#### installing nerdfont
+echo "SETTING UP nerdfont"
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+
+#### installing ripgrep
+echo "SETTING UP ripgrep"
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
+#### installing nvchad
+echo "SETTING UP nvchad"
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+#### adding custom config
+echo "SETTING UP custom config"
+sudo apt-get install -y ninja-build gettext cmake unzip curl
+rm -rf ~/.config/nvim/lua/custom
+git clone https://github.com/ferdinandhubbard981/custom.git ~/.config/nvim/lua/custom
+
+# start nvim
+nvim
--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 rm get-docker.sh
+sudo usermod -aG docker $USER
 # install nvim
-usermod -aG docker $USER
